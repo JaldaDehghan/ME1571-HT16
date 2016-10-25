d67875c7a56d0b633e265be7a55cfc8de93aae62
Mattias Schertell <schertell@gmail.com>
2016-10-25 12:35:49 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,35 @@
 print("hello w0rld");
+
+
+
+
+def web():
+  print("https://python.swaroopch.com/")
+  print("https://learnpythonthehardway.org/book/")
+  print("https://www.codecademy.com/learn/python")
+  print("http://opentechschool.github.io/python-beginners/en/index.html")
+  print("http://docs.python-guide.org/en/latest/")
+  
+  
+def other():
+  print("https://www.google.com/about/careers/students/guide-to-technical-development.html")
+  
+  
+def books():
+  print("http://greenteapress.com/thinkpython/html/index.html")
+  print("http://anandology.com/python-practice-book/index.html")
+  print("http://pymbook.readthedocs.io/en/latest/")
+  
+  
+def video():
+  print("https://www.fullstackpython.com/best-python-videos.html")
+
+def tools():
+  print("https://www.fullstackpython.com/development-environments.html")
+  print("https://packaging.python.org/installing/") #pip
+  print("https://virtualenv.pypa.io/en/stable/") #virtualenv
+  print("https://virtualenvwrapper.readthedocs.io/en/latest/") #virtualenvwrapper
+  print("################################################################################")
+  print("!!!-IMPORTANT-!!!")
+  print("what gives? virtualenv, venv, pyenv, virtualenvwrapper!?!?!?!11!\nwhich one should I use? - you should really read this:")
+  print("https://realpython.com/blog/python/python-virtual-environments-a-primer/")
