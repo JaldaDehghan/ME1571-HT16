8b21ae603cc4766eac76866448b04ae5ef1dc33a
mattische <schertell@gmail.com>
2016-11-01 08:31:52 +0100

    up

--- examples/fahrenheit-celcius.py
+++ examples/fahrenheit-celcius.py
@@ -1,10 +1,9 @@
 f = input("Enter fahrenheit: ")
-f = int(f)
-c =  f - 32 * 5.0/9.0
+c =  round(f - 32 * 5.0/9.0, 1)
+print(str(f) + " degrees fahrenheit is Celcius: " + str(c))
+
+print("-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-")
 
 ce = input("Enter celcius: ")
-
-fa = 9.0/5.0 * ce + 32
-
-print(str(f) + " F in celcius: " + str(c))
-print(str(ce) + " C in fahrenheit: " + str(fa))
+fa = round(9.0/5.0 * ce + 32, 1)
+print(str(ce) + " degrees celcius is Fahrenheit: " + str(fa))
